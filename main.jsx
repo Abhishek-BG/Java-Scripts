3cb4fd3bc050c89b42f100b577ba72e75e696258
codegeeksofficial <abhigowda0190@gmail.com>
2022-08-22 20:10:10 +0530

    Class created

---
+++ main.jsx
@@ -0,0 +1,16 @@
+function Mymap()
+{
+    console.log("Maped")
+}
+Mymap()
+
+class Pro {
+    constructor(props)
+    {
+        super(props);   
+    }
+    render()
+    {
+        return(<h1>hii</h1>);
+    }
+}
